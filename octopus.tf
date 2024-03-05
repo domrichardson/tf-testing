540a5582ed4815a936513daa67d29fba18d22fbd
mrhid6 <dominic.richardson1@hotmail.co.uk>
2024-03-05 12:09:00 +0000

    change runtime type

--- octopus.tf
+++ octopus.tf
@@ -7,11 +7,12 @@ resource "octopusdeploy_ssh_key_account" "ssh-account" {
 }
 
 resource "octopusdeploy_ssh_connection_deployment_target" "od-ssh-target" {
-  name         = "${var.octopus_target_name}_TARGET"
-  fingerprint  = ssh_resource.get_fingerprint.result
-  host         = aws_instance.DR-TF-EC2.public_ip
-  port         = 22
-  environments = ["Environments-1203"]
-  account_id   = octopusdeploy_ssh_key_account.ssh-account.id
-  roles        = ["AWS-EC2"]
+  name                  = "${var.octopus_target_name}_TARGET"
+  fingerprint           = ssh_resource.get_fingerprint.result
+  host                  = aws_instance.DR-TF-EC2.public_ip
+  port                  = 22
+  environments          = ["Environments-1203"]
+  account_id            = octopusdeploy_ssh_key_account.ssh-account.id
+  roles                 = ["AWS-EC2"]
+  dot_net_core_platform = "linux-x64"
 }
