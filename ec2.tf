486789210b3544cc29eb8fd78cdf1869f39442c7
mrhid6 <dominic.richardson1@hotmail.co.uk>
2024-03-05 12:20:45 +0000

    update resource names

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "DR-TF-EC2" {
+resource "aws_instance" "EC2" {
   ami             = var.instance_image
   instance_type   = var.instance_type
   security_groups = [aws_security_group.SG.name]
@@ -12,9 +12,9 @@ resource "aws_instance" "DR-TF-EC2" {
 }
 
 resource "ssh_resource" "get_fingerprint" {
-  host        = aws_instance.DR-TF-EC2.public_ip
+  host        = aws_instance.EC2.public_ip
   private_key = tls_private_key.key.private_key_pem
-  depends_on  = [aws_instance.DR-TF-EC2]
+  depends_on  = [aws_instance.EC2]
   user        = "ubuntu"
   commands = [
     "sudo ssh-keygen -E md5 -lf /etc/ssh/ssh_host_ed25519_key.pub | cut -d' ' -f2 | awk '{ print $1}' | cut -d':' -f2-"
--- octopus.tf
+++ octopus.tf
@@ -9,7 +9,7 @@ resource "octopusdeploy_ssh_key_account" "ssh-account" {
 resource "octopusdeploy_ssh_connection_deployment_target" "od-ssh-target" {
   name                  = "${var.octopus_target_name}_TARGET"
   fingerprint           = ssh_resource.get_fingerprint.result
-  host                  = aws_instance.DR-TF-EC2.public_ip
+  host                  = aws_instance.EC2.public_ip
   port                  = 22
   environments          = ["Environments-1203"]
   account_id            = octopusdeploy_ssh_key_account.ssh-account.id
