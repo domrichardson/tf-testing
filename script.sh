dd731a455d0cdb80cecbe6c6bac3df1a4ff31960
mrhid6 <dominic.richardson1@hotmail.co.uk>
2024-03-05 11:41:23 +0000

    fixed init script

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ sudo apt update -y
 sudo apt upgrade -y
 
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main'
-sudo apt install mono-complete 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF -y
+sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main' -y
+sudo apt install mono-complete mono-runtime -y
